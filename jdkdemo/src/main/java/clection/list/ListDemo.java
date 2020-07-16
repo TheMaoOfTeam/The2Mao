a47efd9e808c6549b0328d334375e464ed6695d7
shuangling.mao <1015952139@qq.com>
2020-07-16 16:38:13 +0800

    add jdkdemo

---
+++ jdkdemo/src/main/java/clection/list/ListDemo.java
@@ -0,0 +1,15 @@
+package clection.list;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author msl on 2020/7/16.
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        List emptyList = Collections.EMPTY_LIST;
+        System.out.println(emptyList);
+        emptyList.add(1);
+    }
+}
