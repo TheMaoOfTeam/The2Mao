54fed74e7e5fef6b546393b937947b187af4a60c
shuangling.mao <1015952139@qq.com>
2021-01-08 16:20:10 +0800

    commit i don't know

--- jdkdemo/src/main/java/thread/future/CompletableFutureDemo.java
+++ jdkdemo/src/main/java/thread/future/CompletableFutureDemo.java
@@ -18,4 +18,41 @@ public class CompletableFutureDemo {
 
         future.thenAccept(System.out::println);
     }
+
+    @Test
+    public void test1() throws InterruptedException {
+        // 两个CompletableFuture执行异步查询:
+        CompletableFuture<String> cfQueryFromSina = CompletableFuture.supplyAsync(() -> {
+            return queryCode("中国石油", "https://finance.sina.com.cn/code/");
+        });
+        CompletableFuture<String> cfQueryFrom163 = CompletableFuture.supplyAsync(() -> {
+            return queryCode("中国石油", "https://money.163.com/code/");
+        });
+
+        // 用anyOf合并为一个新的CompletableFuture:
+        CompletableFuture<Object> cfQuery = CompletableFuture.anyOf(cfQueryFromSina, cfQueryFrom163);
+
+        cfQuery.thenAccept(System.out::println);
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        Thread.sleep(200);
+
+    }
+
+    static String queryCode(String name, String url) {
+        System.out.println("query code from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+        }
+        return "601857";
+    }
+
+    static Double fetchPrice(String code, String url) {
+        System.out.println("query price from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+        }
+        return 5 + Math.random() * 20;
+    }
 }
